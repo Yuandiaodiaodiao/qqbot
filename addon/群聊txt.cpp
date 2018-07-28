2567f45cc3d6831a5215940916a9f5a1142ad850
Yuandiaodiaodiao <qq295087430@outlook.com>
2018-07-28 22:39:58 +0800

    search ban
    水群条数查询
    得知@

--- addon/群聊txt.cpp
+++ addon/群聊txt.cpp
@@ -10,6 +10,7 @@
 #include<map>
 #include<queue>
 #include<fstream>
+#include<random>
 using namespace std;
 using namespace CQ;
 bool debugx = true;//debug模式开关
@@ -18,6 +19,9 @@ long long groupid = 248639786;//群号码
 #define DEBUG(x) if(debugx==true)CQ::sendPrivateMsg(debugqq, ((string(x)).c_str()));
 map<long long, int>mapp;
 map<long long, int>ban;
+std::mt19937 rng;
+std::uniform_int_distribution<int> rand4(2, 4);
+std::uniform_int_distribution<int> rand100(0, 100);
 class users {
 public: users(long long uid, int uspeak) { id = uid; speak = uspeak; }
 		long long id; int speak; string nickname;
@@ -26,6 +30,7 @@ public: users(long long uid, int uspeak) { id = uid; speak = uspeak; }
 		}
 };
 EVE_Enable(Enable) {
+	rng.seed(std::random_device()());
 	fstream f("db.txt",ios::in);
 	if (!f) {
 		f.close();
@@ -57,19 +62,50 @@ EVE_Disable(Disable)
 	return 0;
 }
 
+
+string atinfo = "[CQ:at,qq=1442766687";
 EVE_GroupMsg_EX(Group1) {
+	
+	string msg = eve.message;
+	if (msg.find("稽气人设置@信息") != -1) {
+		atinfo = msg.substr(0, 20);
+		DEBUG("设置@信息为" + atinfo);
+	}
+	if (msg.find("嘤") != -1 && rand4(rng) == 3) {
+		sendGroupMsg(eve.fromGroup, "嘤嘤嘤");
+	}
+	string msg1 = eve.messageRAW;
+	if (msg1.find(atinfo) != -1) {
+		DEBUG("收到@" + msg1);
+		DEBUG(eve.messageRAW);
+	}
+	/*
+	if (rand100(rng) == 60) { 
+		auto msg = eve.sendMsg();
+		msg<<code::image("窥屏.jpg")<<send;
+		DEBUG("表情");
+	}*/
 	if (eve.fromGroup != groupid)return;
 	
 	long long qqid = eve.fromQQ;
-	string msg = eve.message;
-	mapp[qqid]++;
 	
+	mapp[qqid]++;
+
 	if ((msg.find("小可爱") != -1) && (msg.find("水群") != -1)) {
 		ban[qqid]++;
-		if (ban[qqid] >= 2) {
-			setGroupBan(groupid, qqid, 60);
-			string nickname= getGroupMemberInfo(groupid,qqid, true).名片;
-			string sx = "@" + nickname + "你太吵了 歇一会";
+		if (ban[qqid] >= rand4(rng) ){
+			GroupMemberInfo i = getGroupMemberInfo(groupid, qqid, true);
+			
+			string resmsg;
+			if (i.permissions >= 2) {		
+				resmsg="不告诉你";
+			}
+			else {
+				setGroupBan(groupid, qqid, 60);
+				resmsg = "就你话多";
+			}
+			string nickname = i.名片;
+			string sx = "@" + nickname + resmsg;
 			sendGroupMsg(groupid,sx);
 			ban[qqid] = 0;
 			return;
@@ -88,6 +124,12 @@ EVE_GroupMsg_EX(Group1) {
 			strs += "\n"+nickname + "  " + to_string(x.speak) + "条";
 		}
 		long long mesid = sendGroupMsg(groupid, strs);
+	}
+	if (msg.find(atinfo) != -1&&(msg.find("水")!=-1)&&(msg.find("我") != -1)&& (msg.find("记录")!=-1||msg.find("多少") != -1)) {
+		setGroupBan(groupid, qqid, 120);
+		sendGroupMsg(groupid, string(to_string(mapp[qqid])+"条").c_str());
+		
+	
 	}
 
 }
